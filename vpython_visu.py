942e6cab6eb6f22eac1662bec3f27da6ba7b9318
eparisot <eparisot@student.42.fr>
2020-01-14 13:02:49 +0100

    rain + flood OK

--- vpython_visu.py
+++ vpython_visu.py
@@ -1,4 +1,4 @@
-from vpython import scene, quad, sphere, vertex, vec, color
+from vpython import scene, quad, sphere, vertex, vec, color, compound
 
 from collections import deque
 import numpy as np
@@ -23,6 +23,7 @@ def get_color(h):
     return curr_color
 
 def draw_map(xi, yi, zi, n_points):
+    terrain = []
     for row in range(n_points):
         for col in range(n_points):
             if row != n_points-1 and col != n_points-1:
@@ -30,7 +31,19 @@ def draw_map(xi, yi, zi, n_points):
                 b = vertex( pos=vec(xi[row][col+1], yi[row][col+1], zi[row][col+1]), color=get_color(zi[row][col+1]))
                 c = vertex( pos=vec(xi[row+1][col+1], yi[row+1][col+1], zi[row+1][col+1]), color=get_color(zi[row+1][col+1]))
                 d = vertex( pos=vec(xi[row+1][col], yi[row+1][col], zi[row+1][col]), color=get_color(zi[row+1][col]))
-                quad(vs=[a,b,c,d])
+                terrain.append(quad(vs=[a,b,c,d]))
+    compound(terrain)
+    water = []
+    for row in range(n_points):
+        for col in range(n_points):
+            if row != n_points-1 and col != n_points-1:
+                a = vertex( pos=vec(xi[row][col], yi[row][col], 0), color=water_color, opacity=0.3)
+                b = vertex( pos=vec(xi[row][col+1], yi[row][col+1], 0), color=water_color, opacity=0.3)
+                c = vertex( pos=vec(xi[row+1][col+1], yi[row+1][col+1], 0), color=water_color, opacity=0.3)
+                d = vertex( pos=vec(xi[row+1][col], yi[row+1][col], 0), color=water_color, opacity=0.3)
+                water.append(quad(vs=[a,b,c,d]))
+    water_comp = compound(water, pos=vec(n_points / 2, n_points / 2, 0))
+    return water_comp
 
 def is_empty(drops, x, y, z):
     for drop in drops:
@@ -39,39 +52,41 @@ def is_empty(drops, x, y, z):
     return True
 
 def update_drops(drops, levels, zi, n_points):
+    start_flood = False
     for i, drop in enumerate(drops):
         drop_pos = drop.pos
         if drop_pos.z > 0:
-            alt = drop_pos.z - zi[int(drop_pos.y)][int(drop_pos.x)]
+            alt = drop_pos.z - (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
             if alt > 1:
                 drop.pos.z -= 1
             elif alt >= 0.1:
-                drop.pos.z = zi[int(drop_pos.y)][int(drop_pos.x)]
+                drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
             else:
                 if int(drop_pos.y) - 1 >= 0 and \
                         is_empty(drops, int(drop_pos.x), int(drop_pos.y) - 1, zi[int(drop_pos.y) - 1][int(drop_pos.x)]) and \
-                        zi[int(drop_pos.y) - 1][int(drop_pos.x)] + levels[int(drop_pos.y) - 1][int(drop_pos.x)] < drop_pos.z:
+                        (zi[int(drop_pos.y) - 1][int(drop_pos.x)] if levels < zi[int(drop_pos.y) - 1][int(drop_pos.x)] else levels) < drop_pos.z:
                     drop.pos.y -= 1
-                    drop.pos.z = zi[int(drop_pos.y)][int(drop_pos.x)]
+                    drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
                 elif int(drop_pos.y) + 1 < n_points and \
                         is_empty(drops, int(drop_pos.x), int(drop_pos.y) + 1, zi[int(drop_pos.y) + 1][int(drop_pos.x)]) and \
-                        zi[int(drop_pos.y) + 1][int(drop_pos.x)] + levels[int(drop_pos.y) + 1][int(drop_pos.x)] < drop_pos.z:
+                        (zi[int(drop_pos.y) + 1][int(drop_pos.x)] if levels < zi[int(drop_pos.y) + 1][int(drop_pos.x)] else levels) < drop_pos.z:
                     drop.pos.y += 1
-                    drop.pos.z = zi[int(drop_pos.y)][int(drop_pos.x)]
+                    drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
                 elif int(drop_pos.x) - 1 >= 0 and \
                         is_empty(drops, int(drop_pos.x) - 1, int(drop_pos.y), zi[int(drop_pos.y)][int(drop_pos.x) - 1]) and \
-                        zi[int(drop_pos.y)][int(drop_pos.x) - 1] + levels[int(drop_pos.y)][int(drop_pos.x) - 1] < drop_pos.z:
+                        (zi[int(drop_pos.y)][int(drop_pos.x) - 1] if levels < zi[int(drop_pos.y)][int(drop_pos.x) - 1] else levels) < drop_pos.z:
                     drop.pos.x -= 1
-                    drop.pos.z = zi[int(drop_pos.y)][int(drop_pos.x)]
+                    drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
                 elif int(drop_pos.x) + 1 < n_points and \
                         is_empty(drops, int(drop_pos.x) + 1, int(drop_pos.y), zi[int(drop_pos.y)][int(drop_pos.x) + 1]) and \
-                        zi[int(drop_pos.y)][int(drop_pos.x) + 1] + levels[int(drop_pos.y)][int(drop_pos.x) + 1] < drop_pos.z:
+                        (zi[int(drop_pos.y)][int(drop_pos.x) + 1] if levels < zi[int(drop_pos.y)][int(drop_pos.x) + 1] else levels) < drop_pos.z:
                     drop.pos.x += 1
-                    drop.pos.z = zi[int(drop_pos.y)][int(drop_pos.x)]
+                    drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
                 else:
                     drops.pop(i)
                     drop.visible = False
-                    del drop
+                    start_flood = True
+    return start_flood
 
 
 def vpython_draw_landscape(landscape):
@@ -80,6 +95,7 @@ def vpython_draw_landscape(landscape):
     zi = landscape[2]
     n_points = len(zi[0])
 
+    scene.background = vec(0.95, 0.95, 0.95)
     scene.width = 1200
     scene.height = 600
     scene.center = vec(n_points / 2, n_points / 2, 0)
@@ -87,35 +103,25 @@ def vpython_draw_landscape(landscape):
     scene.bind("mousedown", mouseDown)
 
     # draw map
-    draw_map(xi, yi, zi, n_points)
+    water_comp = draw_map(xi, yi, zi, n_points)
 
     drops = []
-    levels = np.zeros((n_points, n_points), dtype="int8")
-    fc = 0
+    levels = 0.0
     while True:
         while not run:
             scene.waitfor('redraw')
         # create drops
         x = np.random.randint(0, n_points)
         y = np.random.randint(0, n_points)
-        drop = sphere(pos=vec(x, y, n_points), radius=1, color=water_color, opacity=0.6)
+        drop = sphere(pos=vec(x, y, n_points), radius=0.5, color=water_color, opacity=0.3)
         drops.append(drop)
         # update drops
-        update_drops(drops, levels, zi, n_points)
-
-        """if fc % 60 == 0:
-            for row in range(n_points):
-                for col in range(n_points):
-                    if row != n_points-1 and col != n_points-1 and levels[row][col] > zi[row][col]:
-                        a = vertex( pos=vec(xi[row][col], yi[row][col], levels[row][col]), color=water_color)
-                        b = vertex( pos=vec(xi[row][col+1], yi[row][col+1], levels[row][col+1]), color=water_color)
-                        c = vertex( pos=vec(xi[row+1][col+1], yi[row+1][col+1], levels[row+1][col+1]), color=water_color)
-                        d = vertex( pos=vec(xi[row+1][col], yi[row+1][col], levels[row+1][col]), color=water_color)
-                        quad(vs=[a,b,c,d])
-            levels += 1
-            fc = 0
-        else:
-            fc += 1"""
+        start_flood = update_drops(drops, levels, zi, n_points)
+        # flood water
+        if start_flood == True and levels < n_points:
+            water_comp.pos.z += 0.01
+            levels += 0.01
+        # TODO wave(s)
 
         # set speed
         time.sleep(1/60)
