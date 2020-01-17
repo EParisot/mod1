ba53298ca2063adbc8bc448b3ac1a4aca77d90a8
eparisot <eparisot@student.42.fr>
2020-01-17 19:50:16 +0100

    camera OK

--- panda3d_visu.py
+++ panda3d_visu.py
@@ -1,8 +1,11 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import Geom, GeomNode, GeomVertexFormat, \
                          GeomVertexData, GeomVertexWriter, \
+                         GeomVertexRewriter, GeomVertexReader, \
                          GeomTriangles, \
-                         DirectionalLight, AmbientLight
+                         DirectionalLight, AmbientLight, \
+                         TransparencyAttrib
+
 import numpy as np
 
 class MyApp(ShowBase):
@@ -10,14 +13,22 @@ class MyApp(ShowBase):
     def __init__(self, landscape, n_points):
         self.x = landscape[0]
         self.y = landscape[1]
-        self.z = landscape[2]
+        self.lz = landscape[2]
+        self.wz = np.ones((n_points, n_points))
         self.n_points = n_points
 
         ShowBase.__init__(self)
+        self.setBackgroundColor(0.9,0.9,0.9)
+        self.trackball.node().setPos(0, 300, -20)
+        self.trackball.node().setHpr(0, 30, 0)
 
-        self.draw_mesh()
+        self.landscape_nodePath = self.draw_landscape_mesh()
+        self.landscape_nodePath.setPos(-50, -50, 0)
+        self.water_nodePath = self.draw_water_mesh()
+        self.water_nodePath.setPos(-50, -50, 0)
         self.create_light()
-
+        self.taskMgr.add(self.animate_water, "water_anim")
+        
 
     def create_light(self):
         # Create Ambient Light
@@ -38,8 +49,9 @@ class MyApp(ShowBase):
         # This light is facing forwards, away from the camera.
         directionalLightNP.setHpr(0, -20, 0)
         render.setLight(directionalLightNP)
+
         
-    def draw_mesh(self):
+    def draw_landscape_mesh(self):
         _format = GeomVertexFormat.get_v3n3cp()
         vdata = GeomVertexData('terrain', _format, Geom.UHStatic)
         vdata.setNumRows(self.n_points**2)
@@ -50,29 +62,24 @@ class MyApp(ShowBase):
 
         for j in range(self.n_points):
             for i in range(self.n_points):
-                vertex.addData3f(self.x[j][i], self.y[j][i], self.z[j][i])
-                if self.z[j][i] < 2:
-                    color.addData4(0.3, 0.2, 0.1, 1)
-                elif self.z[j][i] < 5:
-                    color.addData4(0.8, 0.8, 0.4, 1)
-                elif self.z[j][i] < 50:
+                vertex.addData3f(self.x[j][i], self.y[j][i], self.lz[j][i])
+                if self.lz[j][i] < 2:
+                    color.addData4f(0.3, 0.2, 0.1, 1)
+                elif self.lz[j][i] < 5:
+                    color.addData4f(0.8, 0.8, 0.4, 1)
+                elif self.lz[j][i] < 50:
                     color.addData4f(0, 0.5, 0, 1)
-                elif self.z[j][i] < 70:
+                elif self.lz[j][i] < 70:
                     color.addData4f(0, 0.3, 0, 1)
-                elif self.z[j][i] < 80:
-                    color.addData4(0.7, 0.7, 0.7, 1)
+                elif self.lz[j][i] < 80:
+                    color.addData4f(0.7, 0.7, 0.7, 1)
                 else:
                     color.addData4(0.8, 1, 1, 1)
 
-                if j != self.n_points-1 and i != self.n_points-1:
-                    a = np.array([self.x[j][i], self.y[j][i], self.z[j][i]])
-                    b = np.array([self.x[j][i+1], self.y[j][i+1], self.z[j][i+1]])
-                    c = np.array([self.x[j+1][i], self.y[j+1][i], self.z[j+1][i]])
-                    n = np.cross(b - a, c - a)
-                    norm = n / np.linalg.norm(n)
-                    normal.addData3f(norm[0], norm[1], norm[2])
-                else:
-                    normal.addData3f(0, 0, 1)
+                n = np.array([self.x[j][i], self.y[j][i], self.lz[j][i]])
+                norm = n / np.linalg.norm(n)
+                normal.addData3f(norm[0], norm[1], norm[2])
+
         
         prim = GeomTriangles(Geom.UHStatic)
         for j in range(self.n_points):
@@ -89,8 +96,74 @@ class MyApp(ShowBase):
         geom.addPrimitive(prim)
         node = GeomNode('gnode')
         node.addGeom(geom)
-        nodePath = render.attachNewNode(node)
+        landscape_nodePath = render.attachNewNode(node)
+        return landscape_nodePath
+
+
+    def draw_water_mesh(self):
+        _format = GeomVertexFormat.get_v3n3cp()
+        vdata = GeomVertexData('water', _format, Geom.UHDynamic)
+        vdata.setNumRows(self.n_points**2)
+
+        vertex = GeomVertexWriter(vdata, 'vertex')
+        normal = GeomVertexWriter(vdata, 'normal')
+        color = GeomVertexWriter(vdata, 'color')
+
+        for j in range(self.n_points):
+            for i in range(self.n_points):
+                vertex.addData3f(self.x[j][i], self.y[j][i], self.wz[j][i])
+                color.addData4f(0.3, 0.3, 1, 0.8)
+                n = np.array([self.x[j][i], self.y[j][i], self.wz[j][i]])
+                norm = n / np.linalg.norm(n)
+                normal.addData3f(norm[0], norm[1], norm[2])
         
+        prim = GeomTriangles(Geom.UHDynamic)
+        for j in range(self.n_points):
+            for i in range(self.n_points):
+                if j != self.n_points-1 and i != self.n_points-1:
+                    prim.add_vertices(j*(self.n_points) + i,
+                                        j*(self.n_points) + (i+1),
+                                        (j+1)*(self.n_points) + i)
+                    prim.add_vertices(j*(self.n_points) + (i+1),
+                                        (j+1)*(self.n_points) + (i+1),
+                                        (j+1)*(self.n_points) + i)
+
+        geom = Geom(vdata)
+        geom.addPrimitive(prim)
+        node = GeomNode('gnode')
+        node.addGeom(geom)
+        water_nodePath = render.attachNewNode(node)
+        water_nodePath.setTransparency(TransparencyAttrib.MAlpha)
+        return water_nodePath
+
+    def animate_water(self, task):
+        if task.time + 1 < self.n_points:
+
+            water_node = self.water_nodePath.node()
+            water_geom = water_node.modifyGeom(0)
+            water_data = water_geom.modifyVertexData()
+            
+            water_data.setNumRows(self.n_points**2)
+            vertex = GeomVertexRewriter(water_data, 'vertex')
+            normal = GeomVertexRewriter(water_data, 'normal')
+            for j in range(self.n_points):
+                for i in range(self.n_points):
+                    v = vertex.getData3f()
+                    # flood
+                    self.wz[j][i] = task.time + 1
+                    # waves computation
+                    # TODO
+
+                    # render water
+                    vertex.setData3f(v[0], v[1], self.wz[j][i])
+                    n = np.array([self.x[j][i], self.y[j][i], self.wz[j][i]])
+                    norm = n / np.linalg.norm(n)
+                    normal.setData3f(norm[0], norm[1], norm[2])
+        else:
+            task.done()
+        return task.cont
+            
+    
 def panda3d_draw_landscape(landscape, n_points):
     app = MyApp(landscape, n_points)
     app.run()
---
+++ vpython_visu.py
@@ -0,0 +1,145 @@
+from vpython import scene, quad, sphere, vertex, vec, color, compound, cross
+
+from collections import deque
+import numpy as np
+import time
+
+run = True
+key = ""
+rain = False
+flood = False
+def keyDown(ev):
+    global key, run, rain, flood
+    key = ev.key
+    if key == "r":
+        rain = not rain
+    elif key == "f":
+        flood = not flood
+    if ev.key == " ":
+        run = not run
+
+water_color = vec(0, 0, 0.6)
+
+def get_color(h):
+    if h < 5:
+        curr_color = vec(102, 51, 0) / 255
+    elif h < 10:
+        curr_color = vec(153, 204, 0) / 255
+    elif h < 70:
+        curr_color = vec(0, 153, 0) / 255
+    elif h < 100:
+        curr_color = vec(255, 255, 255) / 255
+    return curr_color
+
+def draw_map(xi, yi, zi, n_points):
+    terrain = []
+    for row in range(n_points-1):
+        for col in range(n_points-1):
+            a = vertex( pos=vec(xi[row][col], yi[row][col], zi[row][col]), color=get_color(zi[row][col]), shininess=0)
+            b = vertex( pos=vec(xi[row][col+1], yi[row][col+1], zi[row][col+1]), color=get_color(zi[row][col+1]), shininess=0)
+            c = vertex( pos=vec(xi[row+1][col+1], yi[row+1][col+1], zi[row+1][col+1]), color=get_color(zi[row+1][col+1]), shininess=0)
+            d = vertex( pos=vec(xi[row+1][col], yi[row+1][col], zi[row+1][col]), color=get_color(zi[row+1][col]), shininess=0)
+            norm_b = - b.pos - a.pos
+            norm_d = - d.pos - a.pos
+            a.normal = cross(norm_b, norm_d)
+            terrain.append(quad(vs=[a,b,c,d]))
+    compound(terrain)
+    water = []
+    for row in range(n_points):
+        for col in range(n_points):
+            if row != n_points-1 and col != n_points-1:
+                a = vertex( pos=vec(xi[row][col], yi[row][col], 0), color=water_color, opacity=0.3)
+                b = vertex( pos=vec(xi[row][col+1], yi[row][col+1], 0), color=water_color, opacity=0.3)
+                c = vertex( pos=vec(xi[row+1][col+1], yi[row+1][col+1], 0), color=water_color, opacity=0.3)
+                d = vertex( pos=vec(xi[row+1][col], yi[row+1][col], 0), color=water_color, opacity=0.3)
+                water.append(quad(vs=[a,b,c,d]))
+    water_comp = compound(water, pos=vec(n_points / 2, n_points / 2, 0))
+    return water_comp
+
+def is_empty(drops, x, y, z):
+    for drop in drops:
+        if drop.pos.x == x and drop.pos.y == y and drop.pos.z == z:
+            return False
+    return True
+
+def update_drops(drops, levels, zi, n_points):
+    start_flood = False
+    for i, drop in enumerate(drops):
+        drop_pos = drop.pos
+        if drop_pos.z >= 0:
+            alt = drop_pos.z - (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
+            if alt > 1:
+                drop.pos.z -= 1
+            elif alt > 0.1:
+                drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
+            else:
+                if int(drop_pos.y) - 1 >= 0 and \
+                        is_empty(drops, int(drop_pos.x), int(drop_pos.y) - 1, zi[int(drop_pos.y) - 1][int(drop_pos.x)]) and \
+                        (zi[int(drop_pos.y) - 1][int(drop_pos.x)] if levels < zi[int(drop_pos.y) - 1][int(drop_pos.x)] else levels) < drop_pos.z:
+                    drop.pos.y -= 1
+                    drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
+                elif int(drop_pos.y) + 1 < n_points and \
+                        is_empty(drops, int(drop_pos.x), int(drop_pos.y) + 1, zi[int(drop_pos.y) + 1][int(drop_pos.x)]) and \
+                        (zi[int(drop_pos.y) + 1][int(drop_pos.x)] if levels < zi[int(drop_pos.y) + 1][int(drop_pos.x)] else levels) < drop_pos.z:
+                    drop.pos.y += 1
+                    drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
+                elif int(drop_pos.x) - 1 >= 0 and \
+                        is_empty(drops, int(drop_pos.x) - 1, int(drop_pos.y), zi[int(drop_pos.y)][int(drop_pos.x) - 1]) and \
+                        (zi[int(drop_pos.y)][int(drop_pos.x) - 1] if levels < zi[int(drop_pos.y)][int(drop_pos.x) - 1] else levels) < drop_pos.z:
+                    drop.pos.x -= 1
+                    drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
+                elif int(drop_pos.x) + 1 < n_points and \
+                        is_empty(drops, int(drop_pos.x) + 1, int(drop_pos.y), zi[int(drop_pos.y)][int(drop_pos.x) + 1]) and \
+                        (zi[int(drop_pos.y)][int(drop_pos.x) + 1] if levels < zi[int(drop_pos.y)][int(drop_pos.x) + 1] else levels) < drop_pos.z:
+                    drop.pos.x += 1
+                    drop.pos.z = (zi[int(drop_pos.y)][int(drop_pos.x)] if levels < zi[int(drop_pos.y)][int(drop_pos.x)] else levels)
+                else:
+                    drops.pop(i)
+                    drop.visible = False
+                    start_flood = True
+    return start_flood
+
+
+def vpython_draw_landscape(landscape):
+    xi = landscape[0]
+    yi = landscape[1]
+    zi = landscape[2]
+    n_points = len(zi[0])
+
+    scene.background = vec(0.95, 0.95, 0.95)
+    scene.width = 1600
+    scene.height = 800
+    scene.center = vec(n_points / 2, n_points / 2, 0)
+    scene.forward = vec(0, 0.5, -0.2)
+    scene.up = vec(0, 0, 1)
+    scene.bind("keydown", keyDown)
+
+
+    # draw map
+    water_comp = draw_map(xi, yi, zi, n_points)
+
+    drops = []
+    levels = 0.0
+    while True:
+        # handle events
+        while not run:
+            scene.waitfor('redraw')
+        if rain == True:
+            # create drops
+            x = np.random.randint(0, n_points)
+            y = np.random.randint(0, n_points)
+            drop = sphere(pos=vec(x, y, n_points), radius=0.5, color=water_color, opacity=0.3)
+            drops.append(drop)
+            # update drops
+            start_flood = update_drops(drops, levels, zi, n_points)
+        if flood == True or rain == True:
+            if flood == True:
+                start_flood = True
+            # flood water
+            if start_flood == True and levels < n_points:
+                water_comp.pos.z += 0.01
+                levels += 0.01
+        # TODO wave(s)
+
+        # set speed
+        time.sleep(1/60)
